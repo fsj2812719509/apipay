91163ec76979c3719059450f0444e2e388f9fa06
yuanquaner <2812719509@qq.com>
2019-06-15 11:34:18 +0800

    pay

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    去支付
                 </div>
 
                 <div class="links">
