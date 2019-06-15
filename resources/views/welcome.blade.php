1b165ca1e40fa9f257b6cab589c01cec80c8e7c3
yuanquaner <2812719509@qq.com>
2019-06-15 11:38:25 +0800

    pay

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    去支付
+                    pay
                 </div>
 
                 <div class="links">
