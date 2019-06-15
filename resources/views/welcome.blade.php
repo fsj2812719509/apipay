b63ecf7ad4d00e926c5550699a499eb44df2b7f9
yuanquaner <2812719509@qq.com>
2019-06-15 10:30:25 +0800

    pay

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>支付</title>
+        <title>pay</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
